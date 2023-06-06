d30fd62dde59d3a8f93a35376e4e591e41e9c5f7
CEbbinghaus <git@cebbinghaus.com>
2023-06-07 01:16:14 +1000

    updated backend with Dockerfile for CI/CD

---
+++ backend/build-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "--- Rust version info ---"
+rustup --version
+rustc --version
+cargo --version
+
+mkdir -p out
+
+echo "--- Building plugin backend ---"
+cargo build --profile docker
+mkdir -p out
+
+mv target/docker/backend out/backend
+
+echo " --- Cleaning up ---"
+# remove root-owned target folder
+cargo clean
+# remove newly-cloned git repo and artifacts
+rm -rf ./ryzenadj
--- backend/build.sh
+++ backend/build.sh
@@ -2,5 +2,5 @@
 
 cargo build --release --features ,$1
 mkdir -p ../bin
-# TODO replace "backend" \/ with binary name
+
 cp ./target/release/backend ../bin/backend

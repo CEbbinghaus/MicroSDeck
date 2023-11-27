c9eae636750051efa1f5cd0d061696dc50289576
CEbbinghaus <git@cebbinghaus.com>
2023-11-28 03:12:16 +1100

    Added auto restarting to the main.py script

--- main.py
+++ main.py
@@ -11,6 +11,31 @@ class Plugin:
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         # startup
-        self.backend_proc = subprocess.Popen([PARENT_DIR + "/bin/backend"])
         while True:
-            await asyncio.sleep(1)
+            self.return_code = None
+            self.backend_proc = subprocess.Popen([PARENT_DIR + "/bin/backend"])
+            
+            while True:
+                if(self.backend_proc.returncode != None):
+                    self.return_code = self.backend_proc.returncode
+                    break
+                await asyncio.sleep(1)
+
+            match self.return_code:
+                # SIGKILL, SIGTERM, The Process was killed by the user
+                case 137 | 143:
+                    break
+                
+                # SIGABRT The program is assumed to have crashed
+                case 134:
+                    print("SIGABRT was returned")
+                    
+                # The Program exited normally? This happens when its cancelled with Ctrl+C or
+                # one of the threads exited without an error. Restart for good measure
+                case 0:
+                    print("Program exited successfully")
+                
+                case _code:
+                    print("Program exited with exit code: " + _code)
+                    break;
+                    

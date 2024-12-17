d8e1769cbd902ca34e6b316b77406b8747e675a7
CEbbinghaus <git@cebbinghaus.com>
2024-12-18 01:56:04 +1100

    Renamed Doc pages

--- docs/components/ChangeLog.tsx
+++ docs/components/ChangeLog.tsx
@@ -2,11 +2,17 @@ import { ReactElement } from "react";
 import React from "react";
 
 function TagWithCommits({ tag, commits }: { tag: string, commits: Commit[] }): ReactElement {
+	let colorStyle = {};
+	if (!tag.match(/\d+\.\d+\.\d+/)) {
+		colorStyle = { color: "#b0b0b0" };
+	}
+
 	return (
 		<div style={{margin: "6px auto"}}>
-			<h3 style={{fontWeight: "bolder"}}>{tag || "Prerelease"}</h3>
+			<h3 style={{ fontWeight: "bolder", ...colorStyle }}>{tag || "Prerelease"}</h3>
 			<ul>
 			{
+				//TODO: categorize them based on semantic commit
 				commits.map(v => (
 					<li>
 						{v.message}

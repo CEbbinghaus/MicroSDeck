9b0057f5b6475ff90d3bec23c69b289dfebf5237
CEbbinghaus <git@cebbinghaus.com>
2024-12-19 01:01:06 +1100

    Added content and improved look of various parts

--- docs/components/ChangeLog.tsx
+++ docs/components/ChangeLog.tsx
@@ -1,24 +1,30 @@
 import { ReactElement } from "react";
 import React from "react";
+import { Logger } from "../../src/Logging";
 
 function TagWithCommits({ tag, commits }: { tag: string, commits: Commit[] }): ReactElement {
-	let colorStyle = {};
-	if (!tag.match(/\d+\.\d+\.\d+/)) {
-		colorStyle = { color: "#b0b0b0" };
+	if (commits.length == 0) {
+		Logger.Warn("Tag {tag} did not contain any commits. Skipping", { tag: JSON.stringify(tag) });
+		return (<></>);
+	}
+
+	if (!tag) {
+		Logger.Warn("Tag is not defined. Skipping", { tag: JSON.stringify(tag) });
+		return (<></>);
 	}
 
 	return (
-		<div style={{margin: "6px auto"}}>
-			<h3 style={{ fontWeight: "bolder", ...colorStyle }}>{tag || "Prerelease"}</h3>
+		<div style={{ margin: "6px auto" }}>
+			<h3 style={{ fontWeight: "bolder" }}>{tag}</h3>
 			<ul>
-			{
+				{
 				//TODO: categorize them based on semantic commit
-				commits.map(v => (
-					<li>
-						{v.message}
-					</li>
-				))
-			}
+					commits.map(v => (
+						<li>
+							{v.message}
+						</li>
+					))
+				}
 			</ul>
 		</div>
 	)

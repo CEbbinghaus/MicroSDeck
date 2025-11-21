9617dd9c61766c5328c75726099fc6fce1fe4840
CEbbinghaus <git@cebbinghaus.com>
2025-11-21 18:04:56 +1100

    Fixed core problem and reverted to working version

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,9 +3,6 @@ set -e
 
 echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
 
-rustup default stable
-rustup target add x86_64-unknown-linux-musl
-
 cd /backend
 
-sudo bash build-docker.sh
+./build-docker.sh
